39a5b2c3a10a53a4d5ba821eccae61e9ad349f99
mathgrad <jscrager@gmail.com>
2022-03-08 19:07:21 -0600

    Update messenger.js
    Added in basic axios calls

--- src/utilities/scripts/api/messenger.js
+++ src/utilities/scripts/api/messenger.js
@@ -2,16 +2,25 @@ function sendMessage(props) {
   if (!props) throw new Error("Send Message Props' empty");
   switch (props.type) {
     case "getUser":
+      axios.get("/user/" + props.user.id).then((res) => console.log(res));
       break;
     case "updateUser":
+      axios.patch("/user/" + props.user.id).then((res) => console.log(res));
       break;
     case "getClass":
+      axios.get("/class/" + props.class.id).then((res) => console.log(res));
+      break;
+    case "getClasses":
+      axios.get("/classes").then((res) => console.log(res));
       break;
     case "updateClass":
+      axios.patch("/class/" + props.class.id).then((res) => console.log(res));
       break;
     case "getSeats":
+      axios.get("/seats/" + props.seats.id).then((res) => console.log(res));
       break;
     case "updateSeats":
+      axios.get("/seats/" + props.seats.id).then((res) => console.log(res));
       break;
     default:
       throw new Error("Send Message Props' lacks a type");
