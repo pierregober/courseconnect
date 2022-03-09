0437b93afc1f297a5359e1e38256096e313f3ddf
mathgrad <jscrager@gmail.com>
2022-03-08 20:50:46 -0600

    Updated Welcome
    Welcome now looks closer to wireframe

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -4,7 +4,6 @@ import { useEffect } from "react";
 export default function Menu(props) {
   return (
     <>
-      <div className="Banner"></div>
       <nav>
         <div className="Menu">
           <Link to="/">Home</Link>
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,11 +2,7 @@ export default function Welcome() {
   document.title = "Welcome";
   return (
     <div>
-      <div>Welcome</div>
-      <div>
-        To celebrate our new year, we added in new classes. Be sure to check our
-        catalogue and ensure you apply while seats last!
-      </div>
+      <div className="WelcomeBanner"></div>
     </div>
   );
 }
