f098772952a50baffcc67ae7a8f6a3b679d6477e
mathgrad <jscrager@gmail.com>
2022-03-13 14:21:00 -0500

    Created Skeleton Classes
    Seats and class calls

---
+++ src/utilities/scripts/api/class.js
@@ -0,0 +1,10 @@
+import { urlbase } from "../utilities";
+import { getState, updateState } from "../state";
+
+const getClass = (props) => {};
+
+const createClass = (props) => {};
+
+const updateClass = (props) => {};
+
+export { getClass, createClass, updateClass };
---
+++ src/utilities/scripts/api/seats.js
@@ -0,0 +1,10 @@
+import { urlbase } from "../utilities";
+import { getState, updateState } from "../state";
+
+const getSeats = (props) => {};
+
+const createSeats = (props) => {};
+
+const updateSeats = (props) => {};
+
+export { getSeats, createSeats, updateSeats };
