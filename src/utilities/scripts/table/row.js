0ccf8a75dd092e658d83eb05f78300f4adebc8d4
mathgrad <jscrager@gmail.com>
2022-03-15 11:30:09 -0500

    Row now displays
    Added in error checks
    Added in a default empty return

--- src/utilities/scripts/table/row.js
+++ src/utilities/scripts/table/row.js
@@ -2,18 +2,25 @@ import sendMessage from "../api/messenger";
 import { generateKey } from "../utilities";
 
 export default function Row(props) {
-  console.log(props, props.name, props.professor, props.content, props.date);
-  return (
-    <div key={generateKey()} className="ClassTable">
-      <div>{props.name.toString() || "None"}</div>
-      <div>{props.professor.toString() || "None"}</div>
-      <div>{props.content.toString() || "None"}</div>
-      <div>{props.date.toString() || "None"}</div>
-      <div>
-        <button onClick={() => sendMessage({ type: "getClasses", class: {} })}>
-          submit
-        </button>
-      </div>
-    </div>
-  );
+  if (props) {
+    if (props.name && props.professor && props.content && props.dateOffer) {
+      return (
+        <div key={generateKey()} className="ClassTable">
+          <div key={generateKey()}>{props.name.toString() || "None"}</div>
+          <div key={generateKey()}>{props.professor.toString() || "None"}</div>
+          <div key={generateKey()}>{props.content.toString() || "None"}</div>
+          <div key={generateKey()}>{props.dateOffer.toString() || "None"}</div>
+          <div key={generateKey()}>
+            <button
+              onClick={() => sendMessage({ type: "getClasses", class: {} })}
+            >
+              submit
+            </button>
+          </div>
+        </div>
+      );
+    }
+    return <></>;
+  }
+  return <></>;
 }
