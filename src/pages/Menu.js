6cd7582213a2814f719b5e7f079d0d53e22cbbce
mathgrad <jscrager@gmail.com>
2022-03-18 11:17:36 -0500

    Update Menu.js
    Menu is now dynamic

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,16 +1,25 @@
 import { Outlet, Link } from "react-router-dom";
-import { useEffect } from "react";
+import { getState, updateState } from "../utilities/scripts/state";
+import { useEffect, useState } from "react";
 
 export default function Menu(props) {
+  const [login, setLogin] = useState(false);
+  const [user, setUser] = useState(getState().user);
+
+  useEffect(() => {
+    if (user) {
+      setLogin(getState().user.login || false);
+    }
+  }, [user, login]);
   return (
     <>
       <nav>
         <div className="Menu">
           <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/class">Class</Link>
-          <Link to="/schedule">Schedule</Link>
-          <Link to="/profile">Profile</Link>
+          {!login ? <Link to="/login">Login</Link> : <></>}
+          {login ? <Link to="/class">Class</Link> : <></>}
+          {login ? <Link to="/schedule">Schedule</Link> : <></>}
+          {login ? <Link to="/profile">Profile</Link> : <></>}
         </div>
       </nav>
       <div className="Logo"></div>
